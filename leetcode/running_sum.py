5b089894158592125ed8d703820cab645854c1b3
SudheerKovvuru <sudheer8131er@gmail.com>
2024-12-11 14:46:47 +0530

    updated solution

--- leetcode/running_sum.py
+++ leetcode/running_sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
+    def runningSum(self, nums):
         ans=[0]*len(nums)
         for i in range(len(nums)):
             ans[i]=sum(nums[:i+1])
