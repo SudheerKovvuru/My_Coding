769fcf6a539b2e95d2b668c54de04078282299da
SudheerKovvuru <sudheer8131er@gmail.com>
2025-08-03 16:44:50 +0530

    added bruteforce solution

--- cses/increasing_array.py
+++ cses/increasing_array.py
@@ -1,3 +1,16 @@
+#BruteForce
+# n=int(input())
+# arr=list(map(int,input().split()))
+# minmoves=0
+# i=1
+# while i<n:
+#     if arr[i-1]>arr[i]:
+#         while arr[i]<arr[i-1]:
+#             arr[i]+=1
+#             minmoves+=1
+#     i+=1
+# print(minmoves)
+
 n=int(input())
 arr=list(map(int,input().split()))
 minmoves=0
